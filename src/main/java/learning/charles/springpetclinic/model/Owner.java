fba2285da9105c232e0c2f5329d88687ad6c08f5
ChArLeS <charleshoong97@gmail.com>
2022-05-20 16:55:35 +0800

    POJO model

---
+++ src/main/java/learning/charles/springpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package learning.charles.springpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/learning/charles/springpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package learning.charles.springpetclinic.model;
+
+public class Vet extends Person {
+
+}
